e131f3c1ec8569ff54c15c598f82ee778e63ae8a
ekankam <eb.kankam26@gmail.com>
2022-03-28 19:33:10 -0500

    Installed tailwind css and configure config file. Completed the bft card ui

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ['./public/index.html', './src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        defaultFont: ['Outfit', 'sans-serif'],
+      },
+      colors: {
+        mainBg: 'hsl(217, 54%, 11%)',
+        cardBg: 'hsl(216, 50%, 16%)',
+        line: 'hsl(215, 32%, 27%)',
+        white: 'hsl(0, 0%, 100%)',
+        softBlue: 'hsl(215, 51%, 70%)',
+        accent: 'hsl(178, 100%, 50%)',
+      },
+      fontSize: {
+        defaultFontSize: '18px',
+      },
+      fontWeight: {
+        300: '300',
+        400: '400',
+        600: '600',
+      },
+    },
+  },
+  plugins: [],
+}
